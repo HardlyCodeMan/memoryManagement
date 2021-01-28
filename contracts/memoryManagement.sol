b29e1b408390e73c0b9b6fa306314391c0019318
HardlyCodeMan <graham@planbit.solutions>
2021-01-28 12:55:30 +0800

    Add: User functions

--- contracts/memoryManagement.sol
+++ contracts/memoryManagement.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: GPL3
 pragma solidity 0.5.12;
+//pragma experimental ABIEncoderV2;
 
 contract memoryManagement {
     struct Flip {
@@ -12,13 +13,13 @@ contract memoryManagement {
         bool win;
     }
     
-    Flip[] Flips;
     
-    mapping (uint => Flip) public gameMap;
+    mapping (address => uint[]) private userFlips;
+    mapping (uint => Flip) private gameMap;
     
     event newFlipEvent(uint _id, address _player, uint _queryId, uint _expected, uint _result, uint _value, bool _win);
     
-    function random() public view returns (uint) {
+    function random() private view returns (uint) {
         // blocktimestamp (now()) modulus by 2 returing the remainder (0 or 1)
         // NOT RANDOM, only appears to be random
         return now %2; 
@@ -32,6 +33,14 @@ contract memoryManagement {
         uint _expected = random();
         uint _result = random();
         uint _value = 100000;
+        
+        // Logic check for game id to prevent users from trying to create flips with the same id
+        if(gameMap[_id].id != 0) {
+            // Game exists, add more data to prevent double
+            // Needs a better fix
+            _id += _winBoolRNG;
+        }
+        
         
         if(_winBoolRNG == _result) {
             _winBool = true;
@@ -47,10 +56,21 @@ contract memoryManagement {
         gameMap[_id].value = _value;
         gameMap[_id].win = _winBool;
         
+        userFlips[msg.sender].push(_id);
+        
         emit newFlipEvent(_id, msg.sender, _queryId, _expected, _result, _value, _winBool);
     }
     
     function getResult(uint _id) public view returns(uint, address, uint, uint, uint, uint, bool) {
+        require(gameMap[_id].id != 0, "No flips found for that users.");
+        
         return(gameMap[_id].id, gameMap[_id].player, gameMap[_id].queryId, gameMap[_id].expected, gameMap[_id].result, gameMap[_id].value, gameMap[_id].win);
+    }
+    
+    function getUserFlips() public view returns(uint[] memory, uint) {
+        uint[] memory tempArray = userFlips[msg.sender];
+        uint gameCount = tempArray.length;
+        
+        return (userFlips[msg.sender], gameCount);
     }
 }
